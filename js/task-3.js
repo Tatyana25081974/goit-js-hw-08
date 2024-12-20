c105206256c21088d9e9fd5f15bb3b3b43823422
Tatyana <tatyanakorabelnikovamama@gmail.com>
2024-12-20 18:44:12 +0200

    add initial files

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.querySelector('#name-input'); // Знаходимо інпут
-const nameOutput = document.querySelector('#name-output'); // Знаходимо спан
-
-nameInput.addEventListener('input', (event) => {
-  const inputValue = event.target.value.trim(); // Очищуємо значення від пробілів
-  nameOutput.textContent = inputValue || 'Anonymous'; // Підставляємо текст або "Anonymous"
-});
